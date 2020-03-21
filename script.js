88bf18d7e1df9a11187ae2c2e5d48403cc254487
WillV1 <wmvanhook@gmail.com>
2020-03-21 17:42:02 -0400

    Updated file for console.log error

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 var passwordLength = prompt("Please choose a password length of 8-128 characters.")
 var lowercaseTypes = prompt("Would you like to include lowercase characters?")
 var uppercaseTypes = prompt("Would you like to include uppercase characters?")
 var numericalTypes = prompt("Would you like to include numerical characters?")
 var specialTypes = prompt("Would you like to include special characters?")
 
-
-
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numerical = "0123456789";
 var specialChar = "!@#$%^&*()-+";
 
-
-
-function writePassword() {
-var password = generatePassword();
-var passwordText = document.querySelector("#password");
+console.log(lowerCase);
 
 var include = '';
 
 if (lowerCaseTypes = "yes"){
-  include.concat(lowerCase);
+  include += (lowerCase);
 }
 
 if (uppercaseTypes = "yes") {
-  include.concat(upperCase);
+  include += (upperCase);
 }
 
-console.log(include);
+console.log(lowerCase);
 
 if (numericalTypes = "yes") {
   include += numerical
@@ -41,16 +34,25 @@ if (specialTypes = "yes") {
   include += specialChar
 }
 
-
 var keyword = ''
 
   for (var i = 0; i < passwordLength; i++){
-    keyword = keyword + include.charAt((Math.floor(Math.random() * include.length)));
+    keyword = keyword + lowercase.charAt((Math.floor(Math.random() * lowercase.length)));
   }
+
+// Write password to the #password input
+
+
+
+function writePassword() {
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+
   passwordText.value = password;
   
 }
 
+writePassword();
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
