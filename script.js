51ed60dc0b8cacb7b99d92586033acfc64cd3008
WillV1 <wmvanhook@gmail.com>
2020-03-22 10:26:20 -0400

    Updated code based on TA comments

--- script.js
+++ script.js
@@ -7,24 +7,23 @@ var uppercaseTypes = prompt("Would you like to include uppercase characters?")
 var numericalTypes = prompt("Would you like to include numerical characters?")
 var specialTypes = prompt("Would you like to include special characters?")
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numerical = "0123456789";
-var specialChar = "!@#$%^&*()-+";
+//function generate() {
+  
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numerical = '0123456789';
+const specialChar = '!@#$%^&*()+?';
 
-console.log(lowerCase);
 
 var include = '';
 
-if (lowerCaseTypes = "yes"){
+if (lowercaseTypes = "yes"){
   include += (lowerCase);
 }
 
 if (uppercaseTypes = "yes") {
   include += (upperCase);
 }
-
-console.log(lowerCase);
 
 if (numericalTypes = "yes") {
   include += numerical
@@ -37,22 +36,24 @@ if (specialTypes = "yes") {
 var keyword = ''
 
   for (var i = 0; i < passwordLength; i++){
-    keyword = keyword + lowercase.charAt((Math.floor(Math.random() * lowercase.length)));
+    keyword = keyword + include.charAt((Math.floor(Math.random() * include.length)));
   }
+
+  console.log(keyword);
+//}
+
 
 // Write password to the #password input
 
 
 
 function writePassword() {
-var password = generatePassword();
-var passwordText = document.querySelector("#password");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  
-}
 
-writePassword();
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
