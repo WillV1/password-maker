208ef83ad091a4f74925edcefe753f18b6198008
WillV1 <wmvanhook@gmail.com>
2020-03-22 16:26:51 -0400

    Updated Javascript code for homework submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+//Establishes prompts for password generation
 var passwordLength = prompt("Please choose a password length of 8-128 characters.")
 var lowercaseTypes = prompt("Would you like to include lowercase characters?")
 var uppercaseTypes = prompt("Would you like to include uppercase characters?")
 var numericalTypes = prompt("Would you like to include numerical characters?")
 var specialTypes = prompt("Would you like to include special characters?")
 
+
+//Function takes answers to prompts to generate password.  JIC, "include" code suggestions provided by https://www.youtube.com/watch?v=i6cX05cyIgc
 function generate() {
   
 const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
@@ -33,24 +37,22 @@ if (specialTypes = "yes") {
   include += specialChar
 }
 
-var keyword = ''
+let keyword = ''
 
   for (var i = 0; i < passwordLength; i++){
     keyword = keyword + include.charAt((Math.floor(Math.random() * include.length)));
   }
-  
-  console.log(keyword);
+
+  return keyword
 }
 
-generate();
+generate ()
 
 function generatePassword(){
   return generate();
 }
 
 // Write password to the #password input
-
-
 
 function writePassword() {
   var password = generatePassword();
